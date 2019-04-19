0d07277ada4feda54bb1fce83179918186e4e44b
coderoot <github@akimov.me>
2019-04-19 19:27:07 +0300

    Add docker installation for ubuntu

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 docker run -i -v '/root:/root' \
-    -e DEFAULT_NODE_IP="$DEFAULT_NODE_IP" -e DEV_ENV="$DEV_ENV" \
+    -e NODE_IP="$NODE_IP" -e DEV_ENV="$DEV_ENV" \
     -e ORCHESTRATOR="$ORCHESTRATOR" -e OPENSTACK_VERSION="$OPENSTACK_VERSION" \
     contrail-dev-control
